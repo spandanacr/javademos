d5c449d2dff02a6c38f2aa1fc9a7cc62114a17cd
Spandana CR <scr@BLR.ALLEGISINDIA.COM>
2023-02-27 23:27:11 +0530

    static and nonstatic

---
+++ javademosproject/src/javademosproject/car.java
@@ -0,0 +1,16 @@
+package javademosproject;
+
+public class car {
+	
+	public String colour;
+	public int noofwheels;
+	
+	public static String manufacturer = "KIA";
+	public static int model =2019;
+	
+	public car() {//if you give private it does not shows
+		colour="black";
+		noofwheels=4;
+	}
+
+}
--- javademosproject/src/javademosproject/javanonstaticdemo.java
+++ javademosproject/src/javademosproject/javanonstaticdemo.java
@@ -1,14 +1,29 @@
 package javademosproject;
 
 public class javanonstaticdemo {
-	public javanonstaticdemo(){
+	public javanonstaticdemo(){ //"constructor" name same as class
 		System.out.println("From constructor");
+	}
+	static{
+		System.out.println("From static-block");
 	}
 	{
 		System.out.println("From non-static-block");
 	}
 	public static void main(String[] args) {
 		 System.out.println("From main method");
-		 javanonstaticdemo jnsd = new javanonstaticdemo();
+		 javanonstaticdemo jnsd1 = new javanonstaticdemo();//calling constructor & creating object
+		 javanonstaticdemo jnsd2 = new javanonstaticdemo();//creating an other object
+		 
+		 car mycar=new car();
+		 System.out.println("noofwheels");
+		 System.out.println(mycar.noofwheels);
+		 System.out.println(car.manufacturer);
+		 System.out.println(mycar.colour);
+		 System.out.println(car.model);
+		 
 }
 }
+
+//whenever the object is created,the non-static block and the constructor is executed
+//even if we remove the constructor we can create the object,because the compiler creates a default constructor
